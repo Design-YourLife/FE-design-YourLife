3075c9541013301ba6f9d2fd08335c13b371d006
sage-jordan <sagemjrodan@gmail.com>
2019-10-17 21:59:36 -0400

    moved activitiesList and added semantic-ui-react

--- life-app/src/components/Activities/ActivityList.js
+++ life-app/src/components/Activities/ActivityList.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axiosWithAuth from '../Authentication/axiosWithAuth';
 
 export default function ActivityList() {
   const [activity, setActivity] = useState([]);
   useEffect(() => {
-    axios
+    axiosWithAuth()
       .get(`https://bw-designyourlife-api.herokuapp.com/api/activities/`)
       .then(res => {
         console.log("Activity: ", res.activity);
         setActivity(res.activity);
       });
   }, []);
-}
+
+  return (
+    <div className="activity-list">
+      
+    </div>
+  )
+};
