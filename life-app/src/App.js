82a72b416f31af5bea1bd9c895b5842beb37eaec
Joshua Starling <joshstarling97@gmail.com>
2019-10-17 21:41:05 -0400

    commit.

--- life-app/src/App.js
+++ life-app/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import ActivityList from "./components/Activities";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" component={ActivityList} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
