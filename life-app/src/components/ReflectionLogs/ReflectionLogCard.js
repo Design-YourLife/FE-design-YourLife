16a252dd85ae64661ed20249dc9b786a98247ae0
sage-jordan <sagemjrodan@gmail.com>
2019-10-21 22:08:30 -0400

    some changes

--- life-app/src/components/ReflectionLogs/ReflectionLogCard.js
+++ life-app/src/components/ReflectionLogs/ReflectionLogCard.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { Card } from 'semantic-ui-react';
 
 export class ReflectionLogCard extends React.Component{
-    constructor(){
+    constructor(props){
         super(props);
         this.state={
-            log: this.props.log
+            // log: this.props.log
         }
     }
 
     render(){
+        console.log("rendering a card");
         return (
         <div className="reflection-card">
             <Card 
-            header={log.log.user_id}
-            meta={log.log.id}
-            description={log.log.reflection}
-            extra={log.log.date}
+            // header={log.log.user_id}
+            // meta={log.log.id}
+            // description={log.log.reflection}
+            // extra={log.log.date}
             />
             <button>Edit</button>
             <button>Delete</button>
--- life-app/src/components/ReflectionLogs/ReflectionLogs.js
+++ life-app/src/components/ReflectionLogs/ReflectionLogs.js
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { axiosWithAuth } from '../Authentication/axiosWithAuth';
-import ReflectionLogCard from './ReflectionLogCard';
+import { ReflectionLogCard } from './ReflectionLogCard';
 
 
 export default function ReflectionLogs () {
     const [ reflectionLogs, setReflectionLogs ] = useState([]);
 
     useEffect(() => {
+        const token = localStorage.getItem('token');
         axiosWithAuth()
-        .get(`https://bw-designyourlife-api.herokuapp.com/api/reflection-logs/user`)
+        .get("/activities/testUser", token)
         .then(res => {
-            console.log("Reflection Log: ", res.data.reflectionLogs);
-            setReflectionLogs(res.data.reflectionLogs);
+            console.log("Reflection Logs: ", res.data);
+            setReflectionLogs(res.data);
         });
     }, [])
     return (
         <div className="reflection-logs">
+            <h1>Reflection Logs</h1>
             {reflectionLogs.map(log => {
                 return <ReflectionLogCard log={log} key={log.id} />
             })}
